2df64020be050388a1f0e61016edff8c1b53ae53
Jerrin Thomas <jerryetm007@gmail.com>
2023-11-02 01:19:20 +0530

    code

--- setup.py
+++ setup.py
@@ -22,6 +22,6 @@ version='0.0.1',
 author='Jerrin',
 author_email='jerryetm007@gmail.com',
 packages=find_packages(),
-install_requires=get_requirements('requirements.txt')
+install_requires=get_requirements('C:\Users\jerry\OneDrive\Desktop\NLP\requirements.txt')
 
 )
